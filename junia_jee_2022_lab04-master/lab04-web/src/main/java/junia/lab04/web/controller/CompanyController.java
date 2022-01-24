253698c1db15da31646712886f9c87b45d4632e9
Imane-C <imane.choukhii@hotmail.fr>
2022-01-24 16:45:07 +0100

    Fill Company Controller class

--- junia_jee_2022_lab04-master/lab04-web/src/main/java/junia/lab04/web/controller/CompanyController.java
+++ junia_jee_2022_lab04-master/lab04-web/src/main/java/junia/lab04/web/controller/CompanyController.java
@@ -1,2 +1,21 @@
-package junia.lab04.web.controller;public class CompanyController {
+package junia.lab04.web.controller;
+
+import junia.lab04.core.service.CompanyService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class CompanyController {
+
+    public CompanyService service;
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String[] getListOfCompanies(ModelMap modelMap){
+
+        service.findAllWithProjects();
+
+        return new String[0];
+    }
 }
