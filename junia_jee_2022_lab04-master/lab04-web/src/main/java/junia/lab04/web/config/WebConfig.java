cb014578ffebe704052aa6f4c53903f8144d1c73
Imane-C <imane.choukhii@hotmail.fr>
2022-01-24 15:49:37 +0100

    Fill WebConfig Class

---
+++ junia_jee_2022_lab04-master/lab04-web/src/main/java/junia/lab04/web/config/WebConfig.java
@@ -0,0 +1,38 @@
+package junia.lab04.web.config;
+
+import com.shieldsolutions.velocity.view.VelocityConfigurer;
+import com.shieldsolutions.velocity.view.VelocityViewResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "junia.lab04.web.controller")
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/");
+    }
+
+    @Bean
+    public com.shieldsolutions.velocity.view.VelocityConfigurer VelocityConfigurer(){
+        VelocityConfigurer velocityConfigurer = new VelocityConfigurer();
+        velocityConfigurer.setResourceLoaderPath("/WEB-INF/velocity");
+        return velocityConfigurer;
+    }
+
+    @Bean
+    public VelocityViewResolver velocityViewResolver() {
+        VelocityViewResolver bean = new VelocityViewResolver();
+        bean.setSuffix(".vm");
+        return bean;
+    }
+
+
+
+}
