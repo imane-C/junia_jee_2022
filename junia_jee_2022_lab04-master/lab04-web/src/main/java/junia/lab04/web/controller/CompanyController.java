263ff32e414ca862f389740df170b1152cb2c449
Imane-C <imane.choukhii@hotmail.fr>
2022-01-24 17:19:13 +0100

    Modify getListOfCompanies() in CompanyController class

--- junia_jee_2022_lab04-master/lab04-web/src/main/java/junia/lab04/web/controller/CompanyController.java
+++ junia_jee_2022_lab04-master/lab04-web/src/main/java/junia/lab04/web/controller/CompanyController.java
@@ -1,10 +1,13 @@
 package junia.lab04.web.controller;
 
+import junia.lab04.core.entity.Company;
 import junia.lab04.core.service.CompanyService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 public class CompanyController {
@@ -14,7 +17,11 @@ public class CompanyController {
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public String[] getListOfCompanies(ModelMap modelMap){
 
-        service.findAllWithProjects();
+        //returns list object
+        List<Company> companyList = service.findAllWithProjects();
+
+        // TO DO : iterate loaded data and append to map
+        //modelMap.put("key", "value");
 
         return new String[0];
     }
