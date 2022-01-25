f2d42f0792d8d0575bce4b51da946fb184f48acd
Antoine Hésèque <antoine.heseque@orange.com>
2022-01-25 20:42:52 +0100

    Update Initializer
    Final version?

--- junia_jee_2022_lab04-master/lab04-web/src/main/java/junia/lab04/web/Initializer.java
+++ junia_jee_2022_lab04-master/lab04-web/src/main/java/junia/lab04/web/Initializer.java
@@ -1,16 +1,20 @@
 package junia.lab04.web;
 
+import junia.lab04.core.config.AppConfig;
+import junia.lab04.core.config.DBConfig;
+import junia.lab04.web.config.WebConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class Initializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+        return new Class[]{ AppConfig.class, DBConfig.class };
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[0];
+        return new Class[]{ WebConfig.class };
     }
 
     @Override
